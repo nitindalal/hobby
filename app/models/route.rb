33f66a52838d60350f181f52870a865f6ddb8a2c
Nitin <nitin@hikeezee.com>
2015-05-26 17:08:33 +0530

    second commit

--- app/models/route.rb
+++ app/models/route.rb
@@ -6,7 +6,15 @@ class Route
   field :drops, type: Array, default: []
   field :stops, type: Array, default: []
 
+  before_save :fetch_ids
+
   def self.planning_engine_element
     PlanningEngine.new
+  end
+
+  def fetch_ids
+    self.pickups = self.pickups.map{|p| p.id}
+    self.drops = self.drops.map{|p| p.id}
+    self.stops = self.stops.map{|p| p.id}
   end
 end
